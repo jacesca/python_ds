35352ff26298c632acf9d45ab90ba2b002d5f2ba
jacesca <72521240+jacesca@users.noreply.github.com>
2022-11-30 20:43:32 -0600

    Update 04_03_Using_stored_weights.py

--- Image Processing with Keras in Python/04_03_Using_stored_weights.py
+++ Image Processing with Keras in Python/04_03_Using_stored_weights.py
@@ -11,174 +11,20 @@ Introduction:
 """
 
 # Import packages                          
-#import pandas            as pd                                                #For loading tabular data
 import numpy             as np                                                #For making operations in lists
-#import matplotlib        as mpl                                               #To format numbers with ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}')) or ax.get_xaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
 import matplotlib.pyplot as plt                                               #For creating charts
-#import seaborn           as sns                                               #For visualizing data
-#import statsmodels       as sm                                                #For stimations in differents statistical models
-#import networkx          as nx                                                #For Network Analysis in Python
-#import nxviz             as nv                                                #For Network Analysis in Python
-
-
-#import scykit-learn                                                           #For performing machine learning  
-#import tabula                                                                 #For extracting tables from pdf
-#import nltk                                                                   #For working with text data
-#import math                                                                   #For accesing to a complex math operations
-#import random                                                                 #For generating random numbers
-#import calendar                                                               #For accesing to a vary of calendar operations
-#import re                                                                     #For regular expressions
-#import timeit                                                                 #For Measure execution time of small code snippets
-#import time                                                                   #To measure the elapsed wall-clock time between two points
-#import warnings
-#import wikipedia
-
-
-#from pandas.plotting                 import register_matplotlib_converters    #For conversion as datetime index in x-axis
-#from math                            import radian                            #For accessing a specific math operations
-#from functools                       import reduce                            #For accessing to a high order functions (functions or operators that return functions)
-#from pandas.api.types                import CategoricalDtype                  #For categorical data
-#from glob                            import glob                              #For using with pathnames matching
-#from datetime                        import date                              #For obteining today function
-#from datetime                        import datetime                          #For obteining today function
-#from string                          import Template                          #For working with string, regular expressions
-#from itertools                       import cycle                             #Used in the function plot_labeled_decision_regions()
-#from math                            import floor                             #Used in the function plot_labeled_decision_regions()
-#from math                            import ceil                              #Used in the function plot_labeled_decision_regions()
-#from itertools                       import combinations                      #For iterations
-#from collections                     import defaultdict                       #Returns a new dictionary-like object
-#from nxviz import ArcPlot                                                     #For Network Analysis in Python
-#from nxviz import CircosPlot                                                  #For Network Analysis in Python 
-#from nxviz import MatrixPlot                                                  #For Network Analysis in Python 
-
-
-#import scipy.stats as stats                                                   #For accesign to a vary of statistics functiosn
-#from scipy.cluster.hierarchy         import fcluster                          #For learning machine - unsurpervised
-#from scipy.cluster.hierarchy         import dendrogram                        #For learning machine - unsurpervised
-#from scipy.cluster.hierarchy         import linkage                           #For learning machine - unsurpervised
 from scipy.signal                    import convolve2d                        #For learning machine - deep learning
-#from scipy.sparse                    import csr_matrix                        #For learning machine 
-#from scipy.special                   import expit as sigmoid                  #For learning machine 
-#from scipy.stats                     import pearsonr                          #For learning machine 
-#from scipy.stats                     import randint                           #For learning machine 
-       
-
-#from sklearn                         import datasets                          #For learning machine
-#from sklearn.cluster                 import KMeans                            #For learning machine - unsurpervised
-#from sklearn.decomposition           import NMF                               #For learning machine - unsurpervised
-#from sklearn.decomposition           import PCA                               #For learning machine - unsurpervised
-#from sklearn.decomposition           import TruncatedSVD                      #For learning machine - unsurpervised
-#from sklearn.ensemble                import AdaBoostClassifier                #For learning machine - surpervised
-#from sklearn.ensemble                import BaggingClassifier                 #For learning machine - surpervised
-#from sklearn.ensemble                import GradientBoostingRegressor         #For learning machine - surpervised
-#from sklearn.ensemble                import RandomForestClassifier            #For learning machine
-#from sklearn.ensemble                import RandomForestRegressor             #For learning machine - unsurpervised
-#from sklearn.ensemble                import VotingClassifier                  #For learning machine - unsurpervised
-#from sklearn.feature_extraction.text import TfidfVectorizer                   #For learning machine - unsurpervised
-#from sklearn.feature_selection       import chi2                              #For learning machine
-#from sklearn.feature_selection       import SelectKBest                       #For learning machine
-#from sklearn.feature_extraction.text import CountVectorizer                   #For learning machine
-#from sklearn.feature_extraction.text import HashingVectorizer                 #For learning machine
-#from sklearn.impute                  import SimpleImputer                     #For learning machine
-#from sklearn.linear_model            import ElasticNet                        #For learning machine
-#from sklearn.linear_model            import Lasso                             #For learning machine
-#from sklearn.linear_model            import LinearRegression                  #For learning machine
-#from sklearn.linear_model            import LogisticRegression                #For learning machine
-#from sklearn.linear_model            import Ridge                             #For learning machine
-#from sklearn.manifold                import TSNE                              #For learning machine - unsurpervised
-#from sklearn.metrics                 import accuracy_score                    #For learning machine
-#from sklearn.metrics                 import classification_report             #For learning machine
-#from sklearn.metrics                 import confusion_matrix                  #For learning machine
-#from sklearn.metrics                 import mean_squared_error as MSE         #For learning machine
-#from sklearn.metrics                 import roc_auc_score                     #For learning machine
-#from sklearn.metrics                 import roc_curve                         #For learning machine
-#from sklearn.model_selection         import cross_val_score                   #For learning machine
-#from sklearn.model_selection         import GridSearchCV                      #For learning machine
-#from sklearn.model_selection         import RandomizedSearchCV                #For learning machine
-#from sklearn.model_selection         import train_test_split                  #For learning machine
-#from sklearn.multiclass              import OneVsRestClassifier               #For learning machine
-#from sklearn.neighbors               import KNeighborsClassifier as KNN       #For learning machine
-#from sklearn.pipeline                import FeatureUnion                      #For learning machine
-#from sklearn.pipeline                import make_pipeline                     #For learning machine - unsurpervised
-#from sklearn.pipeline                import Pipeline                          #For learning machine
-#from sklearn.preprocessing           import FunctionTransformer               #For learning machine
-#from sklearn.preprocessing           import Imputer                           #For learning machine
-#from sklearn.preprocessing           import MaxAbsScaler                      #For learning machine (transforms the data so that all users have the same influence on the model)
-#from sklearn.preprocessing           import Normalizer                        #For learning machine - unsurpervised (for pipeline)
-#from sklearn.preprocessing           import normalize                         #For learning machine - unsurpervised
-#from sklearn.preprocessing           import scale                             #For learning machine
-#from sklearn.preprocessing           import StandardScaler                    #For learning machine
-#from sklearn.svm                     import SVC                               #For learning machine
-#from sklearn.tree                    import DecisionTreeClassifier            #For learning machine - supervised
-#from sklearn.tree                    import DecisionTreeRegressor             #For learning machine - supervised
-
-#import statsmodels.api as sm                                                  #Make a prediction model
-#import statsmodels.formula.api as smf                                         #Make a prediction model    
 import tensorflow as tf                                                       #For DeapLearning
 
-#import keras                                                                 #For DeapLearning
 from keras.callbacks                 import ModelCheckpoint                   #For DeapLearning
-#from keras.callbacks                 import EarlyStopping                     #For DeapLearning
-#from keras.layers                    import BatchNormalization                #For DeapLearning
-#from keras.layers                    import Concatenate                       #For DeapLearning
 from keras.layers                    import Conv2D                            #For DeapLearning
 from keras.layers                    import Dense                             #For DeapLearning
-#from keras.layers                    import Dropout                           #For DeapLearning
-#from keras.layers                    import Embedding                         #For DeapLearning
 from keras.layers                    import Flatten                           #For DeapLearning
-#from keras.layers                    import Input                             #For DeapLearning
 from keras.layers                    import MaxPool2D                         #For DeapLearning
-#from keras.layers                    import Subtract                          #For DeapLearning
-#from keras.models                    import load_model                        #For DeapLearning
-#from keras.models                    import Model                             #For DeapLearning
 from keras.models                    import Sequential                        #For DeapLearning
 from keras.optimizers                import Adam                              #For DeapLearning
-#from keras.optimizers                import SGD                               #For DeapLearning
 from keras.utils                     import plot_model                        #For DeapLearning
 from keras.utils                     import to_categorical                    #For DeapLearning
-
-
-#from bokeh.io                        import curdoc                            #For interacting visualizations
-#from bokeh.io                        import output_file                       #For interacting visualizations
-#from bokeh.io                        import show                              #For interacting visualizations
-#from bokeh.plotting                  import ColumnDataSource                  #For interacting visualizations
-#from bokeh.plotting                  import figure                            #For interacting visualizations
-#from bokeh.layouts                   import row                               #For interacting visualizations
-#from bokeh.layouts                   import widgetbox                         #For interacting visualizations
-#from bokeh.layouts                   import column                            #For interacting visualizations
-#from bokeh.layouts                   import gridplot                          #For interacting visualizations
-#from bokeh.models                    import HoverTool                         #For interacting visualizations
-#from bokeh.models                    import ColumnDataSource                  #For interacting visualizations
-#from bokeh.models                    import CategoricalColorMapper            #For interacting visualizations
-#from bokeh.models                    import Slider                            #For interacting visualizations
-#from bokeh.models                    import Select                            #For interacting visualizations
-#from bokeh.models                    import Button                            #For interacting visualizations
-#from bokeh.models                    import CheckboxGroup                     #For interacting visualizations
-#from bokeh.models                    import RadioGroup                        #For interacting visualizations
-#from bokeh.models                    import Toggle                            #For interacting visualizations
-#from bokeh.models.widgets            import Tabs                              #For interacting visualizations
-#from bokeh.models.widgets            import Panel                             #For interacting visualizations
-#from bokeh.palettes                  import Spectral6                         #For interacting visualizations
-
-
-# Setting the pandas options
-#pd.set_option("display.max_columns",20)
-#pd.options.display.float_format = '{:,.4f}'.format 
-#pd.reset_option("all")
-#register_matplotlib_converters()                                              #Require to explicitly register matplotlib converters.
-
-#plt.rcParams = plt.rcParamsDefault
-#plt.rcParams['figure.constrained_layout.use'] = True
-#plt.rcParams['figure.constrained_layout.h_pad'] = 0.09
-
-#Setting the numpy options
-#np.set_printoptions(precision=3)                                              #precision set the precision of the output:
-#np.set_printoptions(suppress=True)                                            #suppress suppresses the use of scientific notation for small numbers
-#np.set_printoptions(threshold=np.inf)                                         #Show all the columns and rows from an array.
-#np.set_printoptions(threshold=8)                                              #Return to default value.
-
-#Setting images params
-#plt.rcParams.update({'figure.max_open_warning': 0})                           #To solve the max images open
 
 
 print("****************************************************")
